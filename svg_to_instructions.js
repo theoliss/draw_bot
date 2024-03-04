91a153995529218b21df18d8f537f3b9982fb3f1
theoliss <theo.lissarrague@gmail.com>
2024-03-04 09:35:50 +0100

    adding new files

---
+++ svg_to_instructions.js
@@ -0,0 +1,17 @@
+function trad_rect()
+{
+
+}
+
+document.getElementById("done").addEventListener('click', function()
+{
+    if (svgTxt != "")
+    {
+        
+    }
+    else    
+    {
+        console.log("nothing to slice");
+    }
+}
+)
